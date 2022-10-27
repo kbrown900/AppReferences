00351807983ac64c0b574167acc4a5aab3fd853b
kbrown900 <74275228+kbrown900@users.noreply.github.com>
2022-10-27 09:02:37 -0700

    Create WindowsUpdateReset.bat

---
+++ Windows Scripts/WindowsUpdateReset.bat
@@ -0,0 +1,10 @@
+net stop wuauserv 
+net stop cryptSvc 
+net stop bits 
+net stop msiserver 
+Ren C:\Windows\SoftwareDistribution SoftwareDistribution.old 
+Ren C:\Windows\System32\catroot2 Catroot2.old 
+net start wuauserv 
+net start cryptSvc 
+net start bits 
+net start msiserver 
